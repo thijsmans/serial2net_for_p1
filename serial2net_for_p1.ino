aaba0b3079ecf4934d5e3768b149513f218ab7ce
thijsmans <25135711+thijsmans@users.noreply.github.com>
2022-08-04 15:30:58 +0200

    Formatted tabs

--- serial2net_for_p1.ino
+++ serial2net_for_p1.ino
@@ -28,35 +28,35 @@ void setup(void) {
     pinMode( pin_clients[i], OUTPUT);
   }
 
-	// Connect to WiFi network
-	connect_to_wifi();
+  // Connect to WiFi network
+  connect_to_wifi();
 
-	// Start UART
-	Serial.begin(BAUD_RATE);
+  // Start UART
+  Serial.begin(BAUD_RATE);
 
-	// Start server
-	server.begin();
-	server.setNoDelay(true);
+  // Start server
+  server.begin();
+  server.setNoDelay(true);
 }
 
 // Sketch main
 void loop(void)
 {
-	// Check Wifi connection
-	if(WiFi.status() != WL_CONNECTED) 
-	{
-		// Connection lost, close connections
-		for(int i=0; i<MAX_CLIENTS; i++ )
-		{
-			if(serverClients[i])
+  // Check Wifi connection
+  if(WiFi.status() != WL_CONNECTED) 
+  {
+    // Connection lost, close connections
+    for(int i=0; i<MAX_CLIENTS; i++ )
+    {
+      if(serverClients[i])
       {
-				serverClients[i].stop();
+        serverClients[i].stop();
         digitalWrite( pin_clients[i], LOW );
       }
-		}
+    }
    
-		connect_to_wifi();
-	}
+    connect_to_wifi();
+  }
 
   // Remove disconnected clients
   for( int i=0; i<MAX_CLIENTS; i++ )
@@ -68,23 +68,23 @@ void loop(void)
     }
   }
 
-	// Check if there are any new clients
+  // Check if there are any new clients
   if( server.hasClient() )
   {
-	  for( int i=0; i<MAX_CLIENTS; i++ )
-		{
-		  // Find free/disconnected spots
-			if (!serverClients[i] || !serverClients[i].connected())
-			{            
+    for( int i=0; i<MAX_CLIENTS; i++ )
+    {
+      // Find free/disconnected spots
+      if (!serverClients[i] || !serverClients[i].connected())
+      {            
         serverClients[i] = server.available();
         digitalWrite( pin_clients[i], HIGH );
-				continue;
+        continue;
       }
-		}
+    }
    
-		// No free/disconnected spot so reject
-		WiFiClient serverClient = server.available();
-		serverClient.stop();
+    // No free/disconnected spot so reject
+    WiFiClient serverClient = server.available();
+    serverClient.stop();
   }
   
   // Check UART for data
@@ -92,48 +92,46 @@ void loop(void)
   {
     digitalWrite( pin_rx, HIGH );
     
-	  size_t len = Serial.available();
-	  uint8_t sbuf[len];
-		Serial.readBytes(sbuf, len);
+    size_t len = Serial.available();
+    uint8_t sbuf[len];
+    Serial.readBytes(sbuf, len);
     
-		// Push UART data to all connected clients
-		for( int i=0; i<MAX_CLIENTS; i++ )
-		{
+    // Push UART data to all connected clients
+    for( int i=0; i<MAX_CLIENTS; i++ )
+    {
       if(serverClients[i] && serverClients[i].connected())
       {
         digitalWrite( pin_clients[i], LOW );
-		    serverClients[i].write(sbuf, len);
+        serverClients[i].write(sbuf, len);
         digitalWrite( pin_clients[i], HIGH );
-			}
+      }
     }
 
     digitalWrite( pin_rx, LOW );
   }
 }
 
-
 // Functions
 
 void connect_to_wifi() {
-
-	// Is this really needed ?
-	WiFi.mode(WIFI_STA);
-	WiFi.disconnect();
+  /// Is this really needed ?
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
 
   digitalWrite(pin_wifi, LOW);
     
-	delay(50);
+  delay(50);
 
-	// Connect
-	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  // Connect
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-	// Wait for wifi connection
-	while (WiFi.status() != WL_CONNECTED) 
-	{
+  // Wait for wifi connection
+  while (WiFi.status() != WL_CONNECTED) 
+  {
     // toggle led
     digitalWrite( pin_wifi, digitalRead(pin_wifi) ? LOW : HIGH );
     delay(500);
-	}
+  }
 
   // Wifi connection established
   digitalWrite( pin_wifi, HIGH );
